28c9902cacba13dfb592aa8dd45406c28df47089
DISHA-ENTREXT3 <disha@friendsin.entrext.in>
2026-08-19 18:50:18 +0530

    fix: add .js extension to api/index.ts import for node16 moduleResolution (TS2835)

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
 // Vercel Serverless Function entry point
 // Re-exports the Express app from /server/index.ts so Vercel can serve it
 // at the /api/* route prefix defined in vercel.json
-export { default } from "../server/index";
+export { default } from "../server/index.js";
